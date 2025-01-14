fa719f18fa9287b48e006a42b2d311ab5a07d0cf
MadelayHeily-hub <madcodlife@gmail.com>
2025-01-14 20:35:10 +0100

    Ejercicio en visual basic donde te conectes a la BD y recuperes la informacion en una tabla

---
+++ visual_basic/ConexionaBDinicial/ConexionaBDinicial/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btbConectar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnRecuperarInformacion = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btbConectar
+        '
+        Me.btbConectar.Location = New System.Drawing.Point(529, 46)
+        Me.btbConectar.Name = "btbConectar"
+        Me.btbConectar.Size = New System.Drawing.Size(161, 31)
+        Me.btbConectar.TabIndex = 0
+        Me.btbConectar.Text = "Conectar a la BD"
+        Me.btbConectar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Conexión hecha:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(292, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'btnRecuperarInformacion
+        '
+        Me.btnRecuperarInformacion.Location = New System.Drawing.Point(529, 229)
+        Me.btnRecuperarInformacion.Name = "btnRecuperarInformacion"
+        Me.btnRecuperarInformacion.Size = New System.Drawing.Size(170, 29)
+        Me.btnRecuperarInformacion.TabIndex = 3
+        Me.btnRecuperarInformacion.Text = "Recuperar Informacion"
+        Me.btnRecuperarInformacion.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(64, 178)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.Size = New System.Drawing.Size(427, 199)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnRecuperarInformacion)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btbConectar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btbConectar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnRecuperarInformacion As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ visual_basic/ConexionaBDinicial/ConexionaBDinicial/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class Form1
+
+    Dim oconexion As New OleDbConnection
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        oconexion.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\dbempresas.mdb;")
+
+
+    End Sub
+
+    Private Sub btbConectar_Click(sender As Object, e As EventArgs) Handles btbConectar.Click
+
+        Try
+            ' Intentar abrir la conexión
+            oconexion.Open()
+            ' Si se conecta correctamente, mostrar "Sí" en el TextBox
+            TextBox1.Text = "Sí"
+        Catch ex As Exception
+            ' Si ocurre un error, mostrar "No" en el TextBox
+            TextBox1.Text = "No"
+            ' También puedes mostrar un mensaje con más detalles (opcional)
+            MessageBox.Show("Error al conectar: " & ex.Message, "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+
+        End Try
+
+    End Sub
+
+    Private Sub btnRecuperarInformacion_Click(sender As Object, e As EventArgs) Handles btnRecuperarInformacion.Click
+
+        Try
+
+            ' Intentar abrir la conexión
+            oconexion.Open()
+            ' Crear la consulta SQL para obtener los datos de la tabla
+            Dim query As String = "SELECT * FROM tbempresas"
+            Dim comando As New OleDbCommand(query, oconexion)
+
+            ' Crear un adaptador para llenar los datos
+            Dim adaptador As New OleDbDataAdapter(comando)
+            Dim tabla As New DataTable()
+
+            ' Llenar la tabla con los datos de la consulta
+            adaptador.Fill(tabla)
+
+            ' Mostrar los datos en el DataGridView
+            DataGridView1.DataSource = tabla
+
+        Catch ex As Exception
+            ' Mostrar un mensaje de error si ocurre algo
+            MessageBox.Show("Error al recuperar los datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+        End Try
+
+
+
+    End Sub
+
+
+End Class
