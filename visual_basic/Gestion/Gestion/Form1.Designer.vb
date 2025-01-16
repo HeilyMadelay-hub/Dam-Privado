06b5fa1a20cb19c7cdcfa09031cb52fad069bddb
MadelayHeily-hub <madcodlife@gmail.com>
2025-01-16 20:51:23 +0100

    Añadidos los archivos dentro de visual_basic/Gestion/

---
+++ visual_basic/Gestion/Gestion/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonConectar = New System.Windows.Forms.Button()
+        Me.ButtonListar = New System.Windows.Forms.Button()
+        Me.ButtonAlta = New System.Windows.Forms.Button()
+        Me.ButtonBaja = New System.Windows.Forms.Button()
+        Me.ButtonBuscar = New System.Windows.Forms.Button()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxTelefono = New System.Windows.Forms.TextBox()
+        Me.TextBoxDireccion = New System.Windows.Forms.TextBox()
+        Me.TextBoxNombre = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonConectar
+        '
+        Me.ButtonConectar.Location = New System.Drawing.Point(540, 56)
+        Me.ButtonConectar.Name = "ButtonConectar"
+        Me.ButtonConectar.Size = New System.Drawing.Size(80, 32)
+        Me.ButtonConectar.TabIndex = 0
+        Me.ButtonConectar.Text = "Conectar"
+        Me.ButtonConectar.UseVisualStyleBackColor = True
+        '
+        'ButtonListar
+        '
+        Me.ButtonListar.Location = New System.Drawing.Point(540, 111)
+        Me.ButtonListar.Name = "ButtonListar"
+        Me.ButtonListar.Size = New System.Drawing.Size(75, 32)
+        Me.ButtonListar.TabIndex = 1
+        Me.ButtonListar.Text = "Listar"
+        Me.ButtonListar.UseVisualStyleBackColor = True
+        '
+        'ButtonAlta
+        '
+        Me.ButtonAlta.Location = New System.Drawing.Point(540, 164)
+        Me.ButtonAlta.Name = "ButtonAlta"
+        Me.ButtonAlta.Size = New System.Drawing.Size(75, 32)
+        Me.ButtonAlta.TabIndex = 2
+        Me.ButtonAlta.Text = "Alta"
+        Me.ButtonAlta.UseVisualStyleBackColor = True
+        '
+        'ButtonBaja
+        '
+        Me.ButtonBaja.Location = New System.Drawing.Point(540, 214)
+        Me.ButtonBaja.Name = "ButtonBaja"
+        Me.ButtonBaja.Size = New System.Drawing.Size(75, 32)
+        Me.ButtonBaja.TabIndex = 3
+        Me.ButtonBaja.Text = "Baja"
+        Me.ButtonBaja.UseVisualStyleBackColor = True
+        '
+        'ButtonBuscar
+        '
+        Me.ButtonBuscar.Location = New System.Drawing.Point(540, 264)
+        Me.ButtonBuscar.Name = "ButtonBuscar"
+        Me.ButtonBuscar.Size = New System.Drawing.Size(75, 32)
+        Me.ButtonBuscar.TabIndex = 4
+        Me.ButtonBuscar.Text = "Buscar"
+        Me.ButtonBuscar.UseVisualStyleBackColor = True
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.Location = New System.Drawing.Point(540, 317)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(75, 33)
+        Me.ButtonSalir.TabIndex = 5
+        Me.ButtonSalir.Text = "Salir"
+        Me.ButtonSalir.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(59, 34)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(421, 225)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 293)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(70, 343)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Direccion"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(70, 385)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Telefono"
+        '
+        'TextBoxTelefono
+        '
+        Me.TextBoxTelefono.Location = New System.Drawing.Point(179, 381)
+        Me.TextBoxTelefono.Name = "TextBoxTelefono"
+        Me.TextBoxTelefono.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxTelefono.TabIndex = 10
+        '
+        'TextBoxDireccion
+        '
+        Me.TextBoxDireccion.Location = New System.Drawing.Point(179, 340)
+        Me.TextBoxDireccion.Name = "TextBoxDireccion"
+        Me.TextBoxDireccion.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxDireccion.TabIndex = 11
+        '
+        'TextBoxNombre
+        '
+        Me.TextBoxNombre.Location = New System.Drawing.Point(179, 290)
+        Me.TextBoxNombre.Name = "TextBoxNombre"
+        Me.TextBoxNombre.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxNombre.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 464)
+        Me.Controls.Add(Me.TextBoxNombre)
+        Me.Controls.Add(Me.TextBoxDireccion)
+        Me.Controls.Add(Me.TextBoxTelefono)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ButtonSalir)
+        Me.Controls.Add(Me.ButtonBuscar)
+        Me.Controls.Add(Me.ButtonBaja)
+        Me.Controls.Add(Me.ButtonAlta)
+        Me.Controls.Add(Me.ButtonListar)
+        Me.Controls.Add(Me.ButtonConectar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonConectar As Button
+    Friend WithEvents ButtonListar As Button
+    Friend WithEvents ButtonAlta As Button
+    Friend WithEvents ButtonBaja As Button
+    Friend WithEvents ButtonBuscar As Button
+    Friend WithEvents ButtonSalir As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxTelefono As TextBox
+    Friend WithEvents TextBoxDireccion As TextBox
+    Friend WithEvents TextBoxNombre As TextBox
+End Class
---
+++ visual_basic/Gestion/Gestion/Form1.vb
@@ -0,0 +1,278 @@
+﻿
+Imports System.Data
+Imports System.Data.OleDb
+
+Public Class Form1
+
+    Dim oconexion As New OleDbConnection
+    Dim isConnected As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        oconexion.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\dbempresas.mdb;")
+
+    End Sub
+
+    Private Sub btbConectar_Click(sender As Object, e As EventArgs) Handles ButtonConectar.Click
+
+        TextBoxDireccion.Enabled = True
+        TextBoxTelefono.Enabled = True
+
+        Try
+            ' Intentar abrir la conexión
+            oconexion.Open()
+
+            ' Mostrar mensaje de éxito si se conecta correctamente
+            MessageBox.Show("Conexión exitosa", "Conexión", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            isConnected = True
+
+
+
+        Catch ex As Exception
+
+            ' También puedes mostrar un mensaje con más detalles (opcional)
+            MessageBox.Show("Error al conectar: " & ex.Message, "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+
+        End Try
+
+    End Sub
+
+    Private Sub ButtonListar_Click(sender As Object, e As EventArgs) Handles ButtonListar.Click
+
+        TextBoxDireccion.Enabled = True
+        TextBoxTelefono.Enabled = True
+
+        If Not isConnected Then
+
+            MessageBox.Show("Primero debe conectar antes de listar los datos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+            Exit Sub
+
+        End If
+
+        Try
+
+            oconexion.Open()
+
+            'COMANDO
+
+            Dim query As String = "Select * from tbempresas"
+
+            Dim comando As New OleDbCommand(query, oconexion)
+
+            'ADAPTADOR
+
+            Dim adaptador As New OleDbDataAdapter(comando)
+
+            Dim tabla As New DataTable
+
+            'Llenar la tabla
+
+            adaptador.Fill(tabla)
+
+            'Asignar la tabla al datagridview
+
+            DataGridView1.DataSource = tabla
+
+        Catch ex As Exception
+            ' Mostrar mensaje de error en caso de fallo
+            MessageBox.Show("Error al listar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+        End Try
+
+    End Sub
+
+    ' ExecuteNonQuery() se utiliza para ejecutar comandos SQL que modifican datos en la base de datos:
+    ' - INSERT: Añade registros nuevos
+    ' - DELETE: Elimina registros existentes
+    ' - UPDATE: Actualiza registros existentes
+    ' - Comandos DDL (CREATE TABLE, DROP TABLE, etc.): Modifican la estructura de la base de datos
+    ' Devuelve el número de filas afectadas por la operación, pero no devuelve datos.
+    ' Ideal para operaciones que no requieren recuperar información directamente.
+
+
+    Private Sub ButtonAlta_Click(sender As Object, e As EventArgs) Handles ButtonAlta.Click
+
+        TextBoxDireccion.Enabled = True
+        TextBoxTelefono.Enabled = True
+
+        ' Verificar si la conexión está establecida
+        If Not isConnected Then
+            MessageBox.Show("Primero debe conectar antes de realizar esta acción.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Try
+            ' Abrir la conexión
+            oconexion.Open()
+
+            ' Crear la consulta SQL para insertar un registro
+            Dim query As String = "INSERT INTO tbempresas (nombre, direccion, telefono) VALUES (@Nombre, @Direccion, @Telefono)"
+            Dim comando As New OleDbCommand(query, oconexion)
+
+            ' Agregar los parámetros a la consulta
+            comando.Parameters.Add("@Nombre", OleDbType.VarChar).Value = TextBoxNombre.Text
+            comando.Parameters.Add("@Direccion", OleDbType.VarChar).Value = TextBoxDireccion.Text
+            comando.Parameters.Add("@Telefono", OleDbType.VarChar).Value = TextBoxTelefono.Text
+
+            ' Ejecutar el comando
+            Dim filasafectadas As Integer = comando.ExecuteNonQuery()
+
+            ' Confirmar la inserción
+            If filasafectadas > 0 Then
+                MessageBox.Show("Registro insertado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("No se pudo insertar el registro.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+        Catch ex As Exception
+            ' Mostrar mensaje de error
+            MessageBox.Show("Error al insertar el registro: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub ButtonBaja_Click(sender As Object, e As EventArgs) Handles ButtonBaja.Click
+        ' Desactivar los TextBox de Dirección y Teléfono inmediatamente al hacer clic en el botón de Baja
+        TextBoxDireccion.Enabled = False
+        TextBoxTelefono.Enabled = False
+
+        ' Verificar si la conexión está establecida
+        If Not isConnected Then
+            MessageBox.Show("Primero debe conectar antes de realizar esta acción.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Try
+            ' Abrir la conexión
+            oconexion.Open()
+
+            ' Verificar que el usuario ha ingresado un valor clave para eliminar
+            If String.IsNullOrEmpty(TextBoxNombre.Text) Then
+                MessageBox.Show("Debe ingresar el nombre del registro que desea eliminar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Exit Sub
+            End If
+
+            ' Crear la consulta SQL para eliminar un registro de la tabla tbempresas
+            Dim query As String = "DELETE FROM tbempresas WHERE nombre = @Nombre"
+            Dim comando As New OleDbCommand(query, oconexion)
+
+            ' Agregar el parámetro necesario
+            comando.Parameters.Add("@Nombre", OleDbType.VarChar).Value = TextBoxNombre.Text
+
+            ' Ejecutar el comando
+            Dim rowsAffected As Integer = comando.ExecuteNonQuery()
+
+            ' Confirmar la eliminación
+            If rowsAffected > 0 Then
+                MessageBox.Show("Registro eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("No se encontró un registro con el nombre especificado.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+
+        Catch ex As Exception
+            ' Mostrar mensaje de error
+            MessageBox.Show("Error al eliminar el registro: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+        End Try
+
+    End Sub
+
+
+    Private Sub ButtonBuscar_Click(sender As Object, e As EventArgs) Handles ButtonBuscar.Click
+
+        TextBoxDireccion.Enabled = True
+        TextBoxTelefono.Enabled = True
+
+        Dim query As String = "SELECT * FROM tbempresas WHERE 1=1"
+
+        ' Verificar si la conexión está establecida
+        If Not isConnected Then
+            MessageBox.Show("Primero debe conectar antes de buscar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Try
+            ' Abrir la conexión
+            oconexion.Open()
+
+            ' Crear el comando SQL
+            Dim comando As New OleDbCommand(query, oconexion)
+
+            ' Agregar condiciones directamente
+            If Not String.IsNullOrEmpty(TextBoxNombre.Text) Then
+                query &= " AND Nombre = @Nombre"
+                comando.Parameters.Add("@Nombre", OleDbType.VarChar).Value = TextBoxNombre.Text
+            End If
+
+            If Not String.IsNullOrEmpty(TextBoxDireccion.Text) Then
+                query &= " AND Direccion = @Direccion"
+                comando.Parameters.Add("@Direccion", OleDbType.VarChar).Value = TextBoxDireccion.Text
+            End If
+
+            If Not String.IsNullOrEmpty(TextBoxTelefono.Text) Then
+                query &= " AND Telefono = @Telefono"
+                comando.Parameters.Add("@Telefono", OleDbType.VarChar).Value = TextBoxTelefono.Text
+            End If
+
+            ' Actualizar la consulta final en el comando
+            comando.CommandText = query
+
+            ' Crear un DataAdapter para ejecutar la consulta y llenar los datos en un DataTable
+            Dim adaptador As New OleDbDataAdapter(comando)
+            Dim tabla As New DataTable()
+            adaptador.Fill(tabla)
+
+            ' Asignar los resultados al DataGridView
+            DataGridView1.DataSource = tabla
+
+            ' Mostrar los datos en los TextBox si hay resultados
+            If tabla.Rows.Count > 0 Then
+                TextBoxNombre.Text = tabla.Rows(0)("Nombre").ToString()
+                TextBoxDireccion.Text = tabla.Rows(0)("Direccion").ToString()
+                TextBoxTelefono.Text = tabla.Rows(0)("Telefono").ToString()
+            Else
+                ' Mostrar mensaje si no hay resultados
+                MessageBox.Show("No se encontraron resultados para los criterios especificados.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+
+        Catch ex As Exception
+            ' Mostrar mensaje de error
+            MessageBox.Show("Error al buscar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ' Cerrar la conexión si está abierta
+            If oconexion.State = ConnectionState.Open Then
+                oconexion.Close()
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles ButtonSalir.Click
+
+        TextBoxDireccion.Enabled = True
+        TextBoxTelefono.Enabled = True
+
+        Close()
+
+    End Sub
+End Class
