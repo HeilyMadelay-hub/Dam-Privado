5559ff9dd1125f207d63a37b9ad8e7fc0582d9bb
MadelayHeily-hub <madcodlife@gmail.com>
2025-02-02 20:49:40 +0100

    Actualizado

--- java/Tarea22/src/data/Persona.java
+++ java/Tarea22/src/data/Persona.java
@@ -10,29 +10,22 @@ public class Persona implements Serializable{
     private int edad;
     
     public Persona() {
-        super();
-        
+        super();   
     }
-    
     public Persona(String nombre,int edad){
         super();
         this.nombre=nombre;
         this.edad=edad;
-        
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public int getEdad() {
         return edad;
     }
-
     public void setEdad(int edad) {
         this.edad = edad;
     }
